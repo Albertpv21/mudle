f23c0f9c53aa5e22dbfde2f0d76e03155df4ada4
User <User@DESKTOP-0E5BC9V>
2021-11-19 11:26:49 +0200

    Добавьте файлы проекта.

---
+++ mudle/Funktsioonid.py
@@ -0,0 +1,62 @@
+﻿import random 
+def kasutajaandmed():
+    log = input('Kirjutage kasutajanimi ')
+    password = input('Kirjutage parool ')
+    return log, password
+
+def login(kasutajanimed,log,password,passwords):
+    n = len(kasutajanimed)
+    signal = 0
+    for i in range(n):
+        if kasutajanimed[i] == log and passwords[i] == password:
+            print("Autoriseerimine õnnestus. Olete sisse logitud")
+            signal = 1
+        elif kasutajanimed[i] == log and passwords[i] != password:
+            print("Vale parool")
+            signal = -1
+    if signal == 0:      
+        print("Olete uus kasutaja")
+        reg = input("Kas soovite registreeruda? y/n ")
+        if reg == 'n':
+            print("Te ei saa süsteemi kasutada")
+        elif reg == 'y':
+            print("Alustame registreerimisprotsessi")
+            registreerimine(log,password,kasutajanimed,passwords)
+        else:
+            print("Sisestusviga")
+    
+def registreerimine(log,password,kasutajanimed,passwords):
+    kasutajanimed.append(log)
+    print("Olete uus kasutaja")
+    print("Nüüd loome uue parooli")
+    valik = input("Loo parool: a - automaatselt, m - käsitsi: ")
+    if valik == 'a':
+        new_password = passauto()
+        passwords.append(new_password)
+        print(kasutajanimed)
+        print(passwords)
+    elif valik == 'm':
+        new_password = input("Sisestage parool (vähemalt 12 tähemärki) ")
+        n = len(new_password)
+        while n < 12:
+            new_password = input('Liiga lühike parool. Palun sisestage uuesti ')
+            n = len(new_password)
+        passwords.append(new_password)
+    else:
+        print("Sisestusviga")
+
+def passauto()->str:
+    str0= ".,_"
+    str1 = '0123456789'
+    str2 =  'qwertyuiopasdfghjklzxcvbnm'
+    str3 = str2.upper()
+    print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    str4 = str0+str1+str2+str3
+    print(str4)
+    ls = list(str4)
+    print(ls)
+    random.shuffle(ls)
+    print(ls)
+    psword = ''.join([random.choice(ls) for x in range(12)])
+    print(psword)
+    return psword
---
+++ mudle/mudle.py
@@ -0,0 +1,18 @@
+﻿from Funktsioonid import * 
+users=["Albert"]
+passwords=["12345"]
+
+while True:
+    print("Reg-1,Avt-2,Välja-3")
+    valik = input("Millist tahate teha->")
+    if valik == '1':
+        log, password = kasutajaandmed()
+        registreerimine(log,password,users,passwords)
+    elif valik == '2':
+        log, password = kasutajaandmed()
+        login(users,log,password,passwords)
+    elif valik == '3':
+        print("Head aega")
+        break
+    else:
+        print("Vajad kirjutada 1,2,3")
